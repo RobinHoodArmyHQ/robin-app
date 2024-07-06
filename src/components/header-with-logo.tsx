6be71104ebdf6d6944c859cd07c7978bb267543a
Gunjan Patidar <gunjan2307@gmail.com>
2024-07-06 10:55:58 +0000

    feat: onboarding screen

--- src/components/header-with-logo.tsx
+++ src/components/header-with-logo.tsx
@@ -1,16 +1,17 @@
 import * as React from 'react';
+import type { View as ViewType } from 'react-native';
 import { StyleSheet } from 'react-native';
 import { Colors, Image, View } from 'react-native-ui-lib';
 
 const logo = require('@/../assets/icon.png');
 
-export const HeaderWithLogo = (props: React.ComponentProps<any>) => {
+export const HeaderWithLogo = React.forwardRef<ViewType>((props, ref) => {
   return (
-    <View {...props} style={styles.container} center>
+    <View {...props} ref={ref} style={styles.container} center>
       <Image source={logo} aspectRatio={1} style={styles.logo} />
     </View>
   );
-};
+});
 
 const styles = StyleSheet.create({
   container: {
