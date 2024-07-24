0761d55b032ed0970276567133410f7f0f56bb5d
Gunjan Patidar <gunjan2307@gmail.com>
2024-07-24 06:17:03 +0000

    feat: adding EventDetails component

---
+++ src/components/event-details.tsx
@@ -0,0 +1,151 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import openMap from 'react-native-open-maps';
+import { Colors, Text, TouchableOpacity, View } from 'react-native-ui-lib';
+
+import { Icons } from './icons';
+
+export const EventDetails = ({
+  title,
+  description,
+  eventDate,
+  eventStartTime,
+  eventLocation,
+}: {
+  title: string;
+  description: string;
+  eventDate?: Date;
+  eventStartTime?: Date;
+  eventLocation?: {
+    latitude: number;
+    longitude: number;
+    name?: string;
+  };
+}) => {
+  const [descriptionExpanded, setDescriptionExpanded] = React.useState(false);
+
+  let descriptionShortened = description;
+  if (description.length > 150) {
+    descriptionShortened = description.slice(0, 150).trim() + '...';
+  }
+
+  return (
+    <>
+      <Text style={styles.heading}>{title}</Text>
+
+      <View row style={styles.detailsRowContainer}>
+        <View style={styles.iconContainer}>
+          <Icons.Calendar fill={Colors.rhaGreen} />
+        </View>
+        <View>
+          <Text style={styles.detailsText}>
+            {/* 14 December, 2021 &nbsp;&middot;&nbsp; TUESDAY */}
+            {eventDate?.toLocaleDateString([], {
+              weekday: 'long',
+              day: 'numeric',
+              month: 'long',
+              year: 'numeric',
+            })}
+          </Text>
+          <Text style={styles.detailsSubText}>
+            {eventStartTime
+              ?.toLocaleTimeString([], {
+                hour: '2-digit',
+                minute: '2-digit',
+                hour12: true,
+              })
+              .toLocaleUpperCase()
+              .concat(' onwards')}
+          </Text>
+        </View>
+      </View>
+
+      <View row style={styles.detailsRowContainer}>
+        <View style={styles.iconContainer}>
+          <Icons.LocationPin fill={Colors.rhaGreen} />
+        </View>
+        <View>
+          <Text style={styles.detailsText}>
+            {eventLocation?.name ||
+              eventLocation?.latitude.toPrecision(8) +
+                ', ' +
+                eventLocation?.longitude.toPrecision(8)}
+          </Text>
+          <TouchableOpacity
+            onPress={() => {
+              openMap({
+                latitude: eventLocation?.latitude,
+                longitude: eventLocation?.longitude,
+              });
+            }}
+          >
+            <Text marginB-4 underline style={styles.detailsSubText}>
+              View on Map
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View style={styles.subHeading}>
+        <Text style={styles.subHeadingText}>Details</Text>
+      </View>
+      <TouchableOpacity
+        onPress={() => {
+          setDescriptionExpanded(!descriptionExpanded);
+        }}
+      >
+        <Text style={styles.descriptionText}>
+          {descriptionExpanded ? description : descriptionShortened}
+          &nbsp;
+          {descriptionExpanded ? null : <Text underline>read more</Text>}
+        </Text>
+      </TouchableOpacity>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  detailsText: {
+    color: Colors.rhaBlack,
+    fontFamily: 'Poppins_600SemiBold',
+  },
+  detailsSubText: {
+    fontFamily: 'ptsans',
+    color: '#747688',
+  },
+  subHeading: {
+    borderBottomColor: Colors.grey_1,
+    borderBottomWidth: 1,
+    paddingVertical: 4,
+    marginTop: 36,
+    marginBottom: 8,
+  },
+  subHeadingText: {
+    fontFamily: 'PTSans_400Regular',
+    color: Colors.rhaGreen,
+    textTransform: 'uppercase',
+    letterSpacing: 1,
+  },
+  heading: {
+    fontFamily: 'Poppins_600SemiBold',
+    fontSize: 24,
+    lineHeight: 40,
+    color: Colors.rhaBlack,
+  },
+  iconContainer: {
+    backgroundColor: Colors.rgba(Colors.rhaGreen, 0.2),
+    borderRadius: 5,
+    height: 46,
+    aspectRatio: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 16,
+  },
+  detailsRowContainer: {
+    alignItems: 'center',
+    marginTop: 24,
+  },
+  descriptionText: {
+    lineHeight: 24,
+  },
+});
